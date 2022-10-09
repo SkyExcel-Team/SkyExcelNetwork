963f3c4aab52500cf5966602d604604961142370
YooInseo <inseo0121@gmail.com>
2022-10-09 18:15:07 +0900

    translateAlternateColorCodes 적용

--- CashShop/src/main/java/net/skyexcel/server/data/StringData.java
+++ CashShop/src/main/java/net/skyexcel/server/data/StringData.java
@@ -1,137 +1,143 @@
 package net.skyexcel.server.data;
 
 import net.skyexcel.server.SkyExcelNetwork;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 public class StringData {
 
     public static String myCash(Player player) {
         Cash cash = new Cash(player);
-        return getCashMessage("my_cash").replaceAll("%cash%", String.valueOf(cash.getAmount()));
+
+        return ChatColor.translateAlternateColorCodes('&', getCashMessage("my_cash")
+                .replaceAll("%cash%", String.valueOf(cash.getAmount())));
     }
 
     public static String checkPlayerCash(Player target) {
         Cash cash = new Cash(target);
-        return getCashMessage("check_player_cash")
+        return ChatColor.translateAlternateColorCodes('&', getCashMessage("check_player_cash")
                 .replaceAll("%cash%", String.valueOf(cash.getAmount()))
-                .replaceAll("%player%", target.getDisplayName());
+                .replaceAll("%player%", target.getDisplayName()));
     }
 
     public static String sendCash(Player target, int amount) {
 
-        return getCashMessage("check_player_cash")
+        return ChatColor.translateAlternateColorCodes('&', getCashMessage("send_cash")
                 .replaceAll("%cash%", String.valueOf(amount))
-                .replaceAll("%player%", target.getDisplayName());
+                .replaceAll("%player%", target.getDisplayName()));
     }
 
     public static String sendCashAllPlayer(int amount) {
 
-        return getCashMessage("check_player_cash")
-                .replaceAll("%cash%", String.valueOf(amount));
+        return ChatColor.translateAlternateColorCodes('&', getCashMessage("send_cash_all_player")
+                .replaceAll("%cash%", String.valueOf(amount)));
     }
 
     public static String removeCash(Player target, int amount) {
 
-        return getCashMessage("check_player_cash")
+        return ChatColor.translateAlternateColorCodes('&', getCashMessage("remove_cash")
                 .replaceAll("%cash%", String.valueOf(amount))
-                .replaceAll("%player%", target.getDisplayName());
+                .replaceAll("%player%", target.getDisplayName()));
     }
 
     public static String setCash(Player target, int amount) {
 
-        return getCashMessage("set_cash")
+        return ChatColor.translateAlternateColorCodes('&', getCashMessage("set_cash")
                 .replaceAll("%cash%", String.valueOf(amount))
-                .replaceAll("%player%", target.getDisplayName());
+                .replaceAll("%player%", target.getDisplayName()));
     }
 
     public static String resetCash(Player target) {
 
-        return getCashMessage("set_cash")
-                .replaceAll("%player%", target.getDisplayName());
+        return ChatColor.translateAlternateColorCodes('&', getCashMessage("reset_cash")
+                .replaceAll("%player%", target.getDisplayName()));
     }
 
     public static String buyOne(Player target, int price) {
-        return getCashShopMessage("buy_one")
+        return ChatColor.translateAlternateColorCodes('&', getCashShopMessage("buy_one")
                 .replaceAll("%item_order%", target.getDisplayName())
-                .replaceAll("%price%", String.valueOf(price));
+                .replaceAll("%price%", String.valueOf(price)));
     }
 
     public static String sellOne(Player target, int price) {
-        return getCashShopMessage("sell_one")
+        return ChatColor.translateAlternateColorCodes('&', getCashShopMessage("sell_one")
                 .replaceAll("%item_order%", target.getDisplayName())
-                .replaceAll("%price%", String.valueOf(price));
+                .replaceAll("%price%", String.valueOf(price)));
     }
 
     public static String settingBuyPrice() {
-        return getCashShopMessage("setting_buy_price");
+        return ChatColor.translateAlternateColorCodes('&', getCashShopMessage("setting_buy_price"));
     }
 
     public static String settingSellPrice() {
-        return getCashShopMessage("setting_sell_price");
+        return ChatColor.translateAlternateColorCodes('&', getCashShopMessage("setting_sell_price"));
     }
 
     public static String createCashShop(String name) {
-        return getCashShopMessage("create_cashshop")
-                .replaceAll("%cashshop_name%", name);
+        return ChatColor.translateAlternateColorCodes('&', getCashShopMessage("create_cashshop")
+                .replaceAll("%cashshop_name%", name));
     }
 
     public static String recyclebinCashShop(String name) {
-        return getCashShopMessage("recyclebin_cashshop")
-                .replaceAll("%cashshop_name%", name);
+        return ChatColor.translateAlternateColorCodes('&', getCashShopMessage("recyclebin_cashshop")
+                .replaceAll("%cashshop_name%", name));
     }
 
     public static String deleteCashShop(String name) {
-        return getCashShopMessage("delete_cashshop")
-                .replaceAll("%cashshop_name%", name);
+        return ChatColor.translateAlternateColorCodes('&', getCashShopMessage("delete_cashshop")
+                .replaceAll("%cashshop_name%", name));
     }
 
     public static String setGuiSizeCashShop(String name, int size) {
-        return getCashShopMessage("set_gui_size")
+        return ChatColor.translateAlternateColorCodes('&', getCashShopMessage("set_gui_size")
                 .replaceAll("%cashshop_name%", name)
-                .replaceAll("%cashshop_gui_size%", String.valueOf(size));
+                .replaceAll("%cashshop_gui_size%", String.valueOf(size)));
     }
 
 
     public static String setNameCashShop(String original, String newName) {
-        return getCashShopMessage("set_gui_name")
+        return ChatColor.translateAlternateColorCodes('&', getCashShopMessage("set_gui_name")
                 .replaceAll("%cashshop_name%", original)
-                .replaceAll("%set_cashshop_name%", newName);
+                .replaceAll("%set_cashshop_name%", newName));
     }
 
     public static String setBuyPriceCashShop(int price) {
-        return getCashShopMessage("set_buy_price")
-                .replaceAll("%price%", String.valueOf(price));
+        return ChatColor.translateAlternateColorCodes('&', getCashShopMessage("set_buy_price")
+                .replaceAll("%price%", String.valueOf(price)));
     }
 
     public static String overFlow() {
-        return getErrorMessage("overflow");
+        return ChatColor.translateAlternateColorCodes('&', getErrorMessage("overflow"));
     }
 
 
     public static String canNotBuy() {
-        return getErrorMessage("cannot_buy");
+        return ChatColor.translateAlternateColorCodes('&', getErrorMessage("cannot_buy"));
     }
+
     public static String canNotSell() {
-        return getErrorMessage("cannot_sell");
+        return ChatColor.translateAlternateColorCodes('&', getErrorMessage("cannot_sell"));
     }
+
     public static String inventoryFull() {
-        return getErrorMessage("inventoryFull");
+        return ChatColor.translateAlternateColorCodes('&', getErrorMessage("inventoryFull"));
     }
 
     public static String nonePlayer() {
-        return getErrorMessage("command_none_player");
+        return ChatColor.translateAlternateColorCodes('&', getErrorMessage("command_none_player"));
     }
+
     public static String impossibleItemBuy() {
-        return getErrorMessage("impossibleItem_buy");
+        return ChatColor.translateAlternateColorCodes('&', getErrorMessage("impossibleItem_buy"));
     }
 
     public static String impossibleItemSell() {
-        return getErrorMessage("impossibleItem_sell");
+        return ChatColor.translateAlternateColorCodes('&',getErrorMessage("impossibleItem_sell"));
     }
 
 
     public static String noneCash() {
-        return getErrorMessage("command_none_cash");
+        return ChatColor.translateAlternateColorCodes('&',getErrorMessage("command_none_cash"));
     }
 
     private static String getCashMessage(String path) {
