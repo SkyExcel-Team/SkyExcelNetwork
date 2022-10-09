dab0e772c291e3be022e893a959f2bafa3368663
YooInseo <inseo0121@gmail.com>
2022-10-09 18:00:19 +0900

    모든 메세지를 담은 구조체 추가

---
+++ CashShop/src/main/java/net/skyexcel/server/data/StringData.java
@@ -0,0 +1,156 @@
+package net.skyexcel.server.data;
+
+import net.skyexcel.server.SkyExcelNetwork;
+import org.bukkit.entity.Player;
+
+public class StringData {
+
+    public static String myCash(Player player) {
+        Cash cash = new Cash(player);
+        return getCashMessage("my_cash").replaceAll("%cash%", String.valueOf(cash.getAmount()));
+    }
+
+    public static String checkPlayerCash(Player target) {
+        Cash cash = new Cash(target);
+        return getCashMessage("check_player_cash")
+                .replaceAll("%cash%", String.valueOf(cash.getAmount()))
+                .replaceAll("%player%", target.getDisplayName());
+    }
+
+    public static String sendCash(Player target, int amount) {
+
+        return getCashMessage("check_player_cash")
+                .replaceAll("%cash%", String.valueOf(amount))
+                .replaceAll("%player%", target.getDisplayName());
+    }
+
+    public static String sendCashAllPlayer(int amount) {
+
+        return getCashMessage("check_player_cash")
+                .replaceAll("%cash%", String.valueOf(amount));
+    }
+
+    public static String removeCash(Player target, int amount) {
+
+        return getCashMessage("check_player_cash")
+                .replaceAll("%cash%", String.valueOf(amount))
+                .replaceAll("%player%", target.getDisplayName());
+    }
+
+    public static String setCash(Player target, int amount) {
+
+        return getCashMessage("set_cash")
+                .replaceAll("%cash%", String.valueOf(amount))
+                .replaceAll("%player%", target.getDisplayName());
+    }
+
+    public static String resetCash(Player target) {
+
+        return getCashMessage("set_cash")
+                .replaceAll("%player%", target.getDisplayName());
+    }
+
+    public static String buyOne(Player target, int price) {
+        return getCashShopMessage("buy_one")
+                .replaceAll("%item_order%", target.getDisplayName())
+                .replaceAll("%price%", String.valueOf(price));
+    }
+
+    public static String sellOne(Player target, int price) {
+        return getCashShopMessage("sell_one")
+                .replaceAll("%item_order%", target.getDisplayName())
+                .replaceAll("%price%", String.valueOf(price));
+    }
+
+    public static String settingBuyPrice() {
+        return getCashShopMessage("setting_buy_price");
+    }
+
+    public static String settingSellPrice() {
+        return getCashShopMessage("setting_sell_price");
+    }
+
+    public static String createCashShop(String name) {
+        return getCashShopMessage("create_cashshop")
+                .replaceAll("%cashshop_name%", name);
+    }
+
+    public static String recyclebinCashShop(String name) {
+        return getCashShopMessage("recyclebin_cashshop")
+                .replaceAll("%cashshop_name%", name);
+    }
+
+    public static String deleteCashShop(String name) {
+        return getCashShopMessage("delete_cashshop")
+                .replaceAll("%cashshop_name%", name);
+    }
+
+    public static String setGuiSizeCashShop(String name, int size) {
+        return getCashShopMessage("set_gui_size")
+                .replaceAll("%cashshop_name%", name)
+                .replaceAll("%cashshop_gui_size%", String.valueOf(size));
+    }
+
+
+    public static String setNameCashShop(String original, String newName) {
+        return getCashShopMessage("set_gui_name")
+                .replaceAll("%cashshop_name%", original)
+                .replaceAll("%set_cashshop_name%", newName);
+    }
+
+    public static String setBuyPriceCashShop(int price) {
+        return getCashShopMessage("set_buy_price")
+                .replaceAll("%price%", String.valueOf(price));
+    }
+
+    public static String overFlow() {
+        return getErrorMessage("overflow");
+    }
+
+
+    public static String canNotBuy() {
+        return getErrorMessage("cannot_buy");
+    }
+    public static String canNotSell() {
+        return getErrorMessage("cannot_sell");
+    }
+    public static String inventoryFull() {
+        return getErrorMessage("inventoryFull");
+    }
+
+    public static String nonePlayer() {
+        return getErrorMessage("command_none_player");
+    }
+    public static String impossibleItemBuy() {
+        return getErrorMessage("impossibleItem_buy");
+    }
+
+    public static String impossibleItemSell() {
+        return getErrorMessage("impossibleItem_sell");
+    }
+
+
+    public static String noneCash() {
+        return getErrorMessage("command_none_cash");
+    }
+
+    private static String getCashMessage(String path) {
+
+        return SkyExcelNetwork.message.getString("cash_message." + path);
+    }
+
+    private static String getCashShopMessage(String path) {
+
+        return SkyExcelNetwork.message.getString("cashshop_message." + path);
+    }
+
+    private static String getErrorMessage(String path) {
+
+        return SkyExcelNetwork.message.getString("error_message." + path);
+    }
+
+    private static String getOtherMessage(String path) {
+
+        return SkyExcelNetwork.message.getString("other_message." + path);
+    }
+}
