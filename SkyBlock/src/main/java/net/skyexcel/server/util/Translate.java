576d560666a826c48057fa23b247f10eb04f6e79
YooInseo <inseo0121@gmail.com>
2022-10-12 03:24:01 +0900

    Time parseTime 변환 추가

--- SkyBlock/src/main/java/net/skyexcel/server/util/Translate.java
+++ SkyBlock/src/main/java/net/skyexcel/server/util/Translate.java
@@ -19,7 +19,25 @@ public class Translate {
         }
         return string.toString();
     }
-
+    public static String parseTime(long time) {
+        long gameTime = time;
+        long hours = gameTime / 1000 + 6;
+        long minutes = (gameTime % 1000) * 60 / 1000;
+        String ampm = "AM";
+        if (hours >= 12) {
+            hours -= 12;
+            ampm = "오후";
+        }
+        if (hours >= 12) {
+            hours -= 12;
+            ampm = "오전";
+        }
+        if (hours == 0) hours = 12;
+        String mm = "0" + minutes;
+        mm = mm.substring(mm.length() - 2, mm.length());
+        return hours + ":" + mm + " " + ampm;
+    }
+    
     public static String getDate() {
         SimpleDateFormat format2 = new SimpleDateFormat("yyyy년 MM월dd일 HH시mm분ss초");
         Date date = new Date();
