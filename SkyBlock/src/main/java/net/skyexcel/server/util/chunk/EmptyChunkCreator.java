e6d2a6366a644bd3da879efaecaeaebed72be942
YooInseo <inseo0121@gmail.com>
2022-10-13 23:36:52 +0900

    공백 청크 생성기

---
+++ SkyBlock/src/main/java/net/skyexcel/server/util/chunk/EmptyChunkCreator.java
@@ -0,0 +1,14 @@
+package net.skyexcel.server.util.chunk;
+
+import org.bukkit.World;
+import org.bukkit.generator.ChunkGenerator;
+
+import javax.annotation.Nonnull;
+import java.util.Random;
+
+public class EmptyChunkCreator extends ChunkGenerator {
+
+    public ChunkData generateChunkData(@Nonnull World world, @Nonnull Random random, int x, int z, @Nonnull BiomeGrid biome) {
+        return createChunkData(world);
+    }
+}
