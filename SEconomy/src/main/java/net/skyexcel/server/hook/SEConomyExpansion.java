e0a37fb64a7703c6416ab541ac1e7a076419dc2a
YooInseo <inseo0121@gmail.com>
2022-10-19 07:08:28 +0900

    SEconomy PlaceHolder 추가

---
+++ SEconomy/src/main/java/net/skyexcel/server/hook/SEConomyExpansion.java
@@ -0,0 +1,61 @@
+package net.skyexcel.server.hook;
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import net.skyexcel.server.SkyExcelNetwork;
+import net.skyexcel.server.data.economy.SEconomy;
+import net.skyexcel.server.util.Translate;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+public class SEConomyExpansion extends PlaceholderExpansion {
+    private SkyExcelNetwork plugin; // The instance is created in the constructor and won't be modified, so it can be final
+
+    public SEConomyExpansion(SkyExcelNetwork plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public @NotNull String getIdentifier() {
+        return "CashShop";
+    }
+
+    @Override
+    public @NotNull String getAuthor() {
+        return "SkyExcel";
+    }
+
+    @Override
+    public boolean persist() {
+        return true; // This is required or else PlaceholderAPI will unregister the Expansion on reload
+    }
+
+    @Override
+    public @NotNull String getVersion() {
+        return "1.0.0";
+    }
+
+
+    @Override
+    public String getRequiredPlugin() {
+        return "CashShop";
+    }
+
+    @Override
+    public boolean canRegister() {
+        return (plugin = (SkyExcelNetwork) Bukkit.getPluginManager().getPlugin(Objects.requireNonNull(getRequiredPlugin()))) != null;
+    }
+
+    @Override
+    public String onRequest(OfflinePlayer player, String params) {
+        if (params.equalsIgnoreCase("cash")) {
+            SEconomy economy = new SEconomy(player);
+            return Translate.decal(economy.getMoney());
+        }
+
+
+        return null; // Placeholder is unknown by the expansion
+    }
+}
