4aed6d0baa2a434e8f7eb9ce5fafa664bf078f77
YooInseo <inseo0121@gmail.com>
2022-10-18 16:46:51 +0900

    SkyBlockData 객체 생성

---
+++ SkyBlock/src/main/java/net/skyexcel/server/data/SkyBlockData.java
@@ -0,0 +1,10 @@
+package net.skyexcel.server.data;
+
+import net.skyexcel.server.ui.title.Loading;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class SkyBlockData {
+    public static HashMap<UUID, Loading> loading = new HashMap<>();
+}
