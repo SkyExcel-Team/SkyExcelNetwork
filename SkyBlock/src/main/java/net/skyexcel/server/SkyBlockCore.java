8251f8cf4fa385ada6dced0eaa72048481260bc2
YooInseo <inseo0121@gmail.com>
2022-10-25 19:34:40 +0900

    메인 클래스 이름 수정

--- SkyBlock/src/main/java/net/skyexcel/server/SkyBlockCore.java
+++ SkyBlock/src/main/java/net/skyexcel/server/SkyBlockCore.java
@@ -1,26 +1,23 @@
 package net.skyexcel.server;
 
-import net.skyexcel.server.event.BlockBreak;
-import net.skyexcel.server.event.SkyBlockEvent;
-import net.skyexcel.server.event.banBlockEvent;
-import net.skyexcel.server.event.onJoin;
+import net.skyexcel.server.event.*;
 import net.skyexcel.server.hook.SkyBlockExpansion;
 import net.skyexcel.server.island.IslandAdminCmdTab;
 import net.skyexcel.server.island.IslandCmd;
-import net.skyexcel.server.island.IslandCmdTab;
 import net.skyexcel.server.island.OtherCmd;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.World;
 import org.bukkit.event.Listener;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import skyexcel.data.file.Config;
 
+import java.io.File;
 import java.util.Arrays;
 
-public class SkyExcelNetwork extends JavaPlugin implements Listener {
+public class SkyBlockCore extends JavaPlugin implements Listener {
 
-    public static Plugin plugin;
+    public static SkyBlockCore plugin;
 
     @Override
     public void onEnable() {
@@ -28,25 +25,28 @@ public class SkyExcelNetwork extends JavaPlugin implements Listener {
         plugin = this;
 
         init();
+
+
     }
 
     public void init() {
         new IslandCmd();
-        new IslandCmdTab();
+
         new OtherCmd();
         new IslandAdminCmdTab();
 
         new SkyBlockExpansion(this).register();
 
-        Listener[] listeners = {new BlockBreak(), new onJoin(), new SkyBlockEvent(), new banBlockEvent()};
+        Listener[] listeners = {new BlockBreak(), new onJoin(), new SkyBlockEvent(), new banBlockEvent(), new onHit()};
         Arrays.stream(listeners).forEach(listener -> {
             Bukkit.getPluginManager().registerEvents(listener, this);
         });
 
-        Config config = new Config("world/");
-        config.setPlugin(this);
-        for (String name : config.fileListName()) {
-            System.out.println(ChatColor.ITALIC + name + ChatColor.GREEN + " 로드 완료!");
+
+        for (World world : Bukkit.getWorlds()) {
+            
+            System.out.println(ChatColor.GREEN + world.getName() + " 로드 완료!");
         }
+
     }
 }
