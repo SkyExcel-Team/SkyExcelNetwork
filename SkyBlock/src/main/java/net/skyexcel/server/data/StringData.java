13918b356df727b5ceca45e72161085a0f3797d9
YooInseo <inseo0121@gmail.com>
2022-10-25 19:37:13 +0900

    버튼 이름 추가

--- SkyBlock/src/main/java/net/skyexcel/server/data/StringData.java
+++ SkyBlock/src/main/java/net/skyexcel/server/data/StringData.java
@@ -2,7 +2,7 @@ package net.skyexcel.server.data;
 
 public class StringData {
 
-    public static final String NextPageName = "";
+    public static final String NextPageName = "다음 페이지";
 
-    public static final String PreviousPageName = "";
+    public static final String PreviousPageName = "이전 페이지";
 }
