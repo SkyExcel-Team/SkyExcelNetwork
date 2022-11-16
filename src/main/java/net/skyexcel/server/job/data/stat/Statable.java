2797be09fad85a2ed9887f638dc63d768f6bdaee
SkyExcel <inseo0121@gmail.com>
2022-11-16 18:03:17 +0900

    refactor: edit Stat (Job)

--- src/main/java/net/skyexcel/server/job/data/stat/Statable.java
+++ src/main/java/net/skyexcel/server/job/data/stat/Statable.java
@@ -1,10 +1,16 @@
 package net.skyexcel.server.job.data.stat;
 
+import skyexcel.data.file.Config;
+
 public class Statable {
 
     private int level;
 
     private String name;
+
+
+
+
 
     public Statable(String name) {
         this.name = name;
@@ -13,6 +19,10 @@ public class Statable {
 
     public int getLevel() {
         return level;
+    }
+
+    public void setStat(){
+
     }
 
     public String getName() {
--- src/main/java/net/skyexcel/server/job/data/stat/WaterBucket.java
+++ src/main/java/net/skyexcel/server/job/data/stat/WaterBucket.java
@@ -1,11 +1,28 @@
 package net.skyexcel.server.job.data.stat;
 
+import org.bukkit.Bukkit;
+import org.bukkit.inventory.Inventory;
+
 public class WaterBucket extends Statable{
 
+
+
+    private Inventory inv;
+
+    private final int line = 9; //
+
+    private int level = 10; // 4/ 10 = 2
 
 
     public WaterBucket() {
         super("물병");
     }
+
+    public void onGUI(){
+        inv = Bukkit.createInventory(null, 45, "특별한 물병");
+
+
+    }
+
 
 }
