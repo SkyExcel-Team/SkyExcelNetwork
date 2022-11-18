e277ad02fb4aa41640c1e0528cb6e7310311045c
YooInseo <inseo0121@gmail.com>
2022-11-18 20:02:25 +0900

    feat: System update (JobCMD)

--- src/main/java/net/skyexcel/server/job/cmd/FisherCmd.java
+++ src/main/java/net/skyexcel/server/job/cmd/FisherCmd.java
@@ -3,7 +3,7 @@ package net.skyexcel.server.job.cmd;
 import net.skyexcel.server.job.data.Job;
 import net.skyexcel.server.job.data.JobData;
 import net.skyexcel.server.job.data.JobType;
-import net.skyexcel.server.job.data.stat.WaterBucket;
+import net.skyexcel.server.job.data.fisher.WaterBucket;
 import net.skyexcel.server.job.data.type.Fisher;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -21,9 +21,14 @@ public class FisherCmd implements CommandExecutor {
             job.setJobType(JobType.FISHERMAN);
             if (job.getType().equals(JobType.FISHERMAN)) {
 
-                WaterBucket waterBucket = new WaterBucket();
-                waterBucket.onGUI(player);
-                JobData.waterBucket.put(player.getUniqueId(), waterBucket);
+                WaterBucket waterBucket = new WaterBucket(player);
+                if (waterBucket.getLevel() != 0) {
+                    waterBucket.onGUI(player);
+                    JobData.waterBucket.put(player.getUniqueId(), waterBucket);
+                } else {
+                    player.sendMessage("물통 레벨이 0이다 애송아");
+                }
+
             } else {
                 Fisher fisher = new Fisher();
                 player.sendMessage(fisher.getDisplayName() + " 직업만 사용 가능한 기능입니다.");
