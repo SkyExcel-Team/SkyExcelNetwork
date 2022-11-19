67e38241d7efa6283844745f25eed56d385cf270
yejunho10 <hyjcompany30@naver.com>
2022-11-19 15:52:08 +0900

    fix: typo

--- src/main/java/net/skyexcel/server/essentials/farmprotect/SkyExcelNetworkFarmProtectMain.java
+++ src/main/java/net/skyexcel/server/essentials/farmprotect/SkyExcelNetworkFarmProtectMain.java
@@ -1,19 +1,17 @@
 package net.skyexcel.server.essentials.farmprotect;
 
-import net.skyexcel.server.essentials.SkyExcelNetworkEssentialsMain;
 import net.skyexcel.server.essentials.farmprotect.cmd.FarmProtectCmdTabComplete;
 import net.skyexcel.server.essentials.farmprotect.cmd.FarmProtectCommand;
 import net.skyexcel.server.essentials.farmprotect.event.FarmBreakEvent;
 import net.skyexcel.server.essentials.farmprotect.event.JoinEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import skyexcel.data.file.Config;
 
 public class SkyExcelNetworkFarmProtectMain {
     public static JavaPlugin plugin;
 
     public SkyExcelNetworkFarmProtectMain(JavaPlugin plugin) {
-        SkyExcelNetworkEssentialsMain.plugin = plugin;
+        SkyExcelNetworkFarmProtectMain.plugin = plugin;
 
         onEnable();
     }
