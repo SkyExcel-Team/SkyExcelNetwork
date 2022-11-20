1f08096a950d536712b5794ad8f69508ba62a2d5
yejunho10 <hyjcompany30@naver.com>
2022-11-20 22:39:12 +0900

    feat: add `FIREWORKS_SPARK` particle

--- src/main/java/net/skyexcel/server/snowy/scheduler/SnowParticleScheduler.java
+++ src/main/java/net/skyexcel/server/snowy/scheduler/SnowParticleScheduler.java
@@ -14,7 +14,7 @@ import java.util.Random;
 
 public class SnowParticleScheduler extends BukkitRunnable {
     public static int taskId;
-    private static Random random = new Random();
+    private static final Random random = new Random();
 
     private static final String worldName = "lobby"; //TODO - CHANGE
     private static final int centerX = -2;
@@ -28,12 +28,12 @@ public class SnowParticleScheduler extends BukkitRunnable {
     private List<Block> blocks = new ArrayList<>();
 
     public SnowParticleScheduler() {
-        this.taskId = Bukkit.getScheduler().runTaskLaterAsynchronously(SkyExcelNetworkMain.getPlugin(), () -> {
+        taskId = Bukkit.getScheduler().runTaskLaterAsynchronously(SkyExcelNetworkMain.getPlugin(), () -> {
             while(Bukkit.getWorld(worldName) == null) {}
 
             Location location = new Location(Bukkit.getWorld(worldName), centerX, 0, centerZ);
             radius = 40;
-            for (int y = 63; y <= 80; y++) {
+            for (int y = 80; y >= 63; y--) {
                 for (int x = location.getBlockX() - radius; x <= location.getBlockX() + radius; x++) {
                     for (int z = location.getBlockZ() - radius; z <= location.getBlockZ() + radius; z++) {
                         Block block = location.getWorld().getBlockAt(x, y, z);
@@ -78,7 +78,13 @@ public class SnowParticleScheduler extends BukkitRunnable {
                             ParticleEffect.END_ROD.display(loc, player);
                         }
                     });
-                } else {
+                } else if (getBoolean()) {
+                    Bukkit.getWorld(worldName).getPlayers().forEach(player -> {
+                        if ((new SnowToggleData(player)).getConfig().getBoolean("snow")) {
+                            ParticleEffect.FIREWORKS_SPARK.display(loc, player);
+                        }
+                    });
+                } else if (getBoolean()) {
                     Bukkit.getWorld(worldName).getPlayers().forEach(player -> {
                         if ((new SnowToggleData(player)).getConfig().getBoolean("snow")) {
                             ParticleEffect.WHITE_ASH.display(loc, player);
