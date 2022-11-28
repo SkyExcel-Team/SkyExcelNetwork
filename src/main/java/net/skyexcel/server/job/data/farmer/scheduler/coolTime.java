95aa5804c85ad9c01b6e00f82766e8a42d8184b0
YooInseo <inseo0121@gmail.com>
2022-11-28 20:50:35 +0900

    fix: Remove ArmorStand when unload chunk

--- src/main/java/net/skyexcel/server/job/data/farmer/scheduler/coolTime.java
+++ src/main/java/net/skyexcel/server/job/data/farmer/scheduler/coolTime.java
@@ -1,5 +1,6 @@
 package net.skyexcel.server.job.data.farmer.scheduler;
 
+
 import net.skyexcel.server.job.data.JobData;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.ArmorStand;
@@ -27,12 +28,10 @@ public class coolTime extends BukkitRunnable {
     @Override
     public void run() {
         if (time.MINUTE() == 0 && time.SECOND() == 0) {
-            System.out.println(player.getName() + " 님의 허수아비 사라짐.");
 
-            //TODO 플레이어가 아에 없을땐 이 메소드가 작동이 안됨..... 이건 뭐 버킷 api 문제라
-            // Location 변수를 콘피그에 넣어서 거기에 Armorstand가 있으면 remove 해야될듯? (서버가 켜질때)
-            // 해당 아머스텐드 청크
+            armorStand.getWorld().getChunkAt((int) (armorStand.getLocation().getX() / 16), (int) (armorStand.getLocation().getZ() / 16));
             armorStand.remove();
+
             cancel();
             JobData.scarecrow.remove(player.getUniqueId());
         } else {
