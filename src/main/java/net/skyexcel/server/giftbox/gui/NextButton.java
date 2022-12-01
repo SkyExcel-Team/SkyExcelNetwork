165f601ce15c44c418eb5785e395eb8cbc079c67
SkyExcel <inseo0121@gmail.com>
2022-12-01 13:09:46 +0900

    feat: Change Button Icon

--- src/main/java/net/skyexcel/server/giftbox/gui/NextButton.java
+++ src/main/java/net/skyexcel/server/giftbox/gui/NextButton.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class NextButton extends Items {
     public NextButton() {
-        getHeadItemFromHDB("700");
+        getHeadItemFromHDB("701");
         setCustomModelData(1);
         setDisplay("§8[ §7다음페이지 §8] §c→");
         setLore(List.of("§f架 §7클릭시 다음 페이지로 넘어갑니다"));
--- src/main/java/net/skyexcel/server/giftbox/gui/PreviousButton.java
+++ src/main/java/net/skyexcel/server/giftbox/gui/PreviousButton.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class PreviousButton extends Items {
     public PreviousButton() {
         super("");
-        getHeadItemFromHDB("701");
+        getHeadItemFromHDB("700");
         setCustomModelData(1);
         setDisplay("§c← §8[ §7이전페이지 §8]");
         setLore(List.of("§f架 §7클릭시 이전 페이지로 돌아갑니다"));
